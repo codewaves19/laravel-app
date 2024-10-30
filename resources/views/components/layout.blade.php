29ecac1f78e6cf33d89fd731b5c5431bbb9e1c8b
manisha <dayalmanisha14@gmail.com>
2024-10-31 01:52:35 +0530

    Playing with views

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Contact</title>
+    </head>
+    <body>
+        <nav>
+            <a href='/'>Home</a>
+            <a href='/about'>about</a>
+            <a href='/contact'>contact</a>
+        </nav>
+        {{ $slot }}
+    </body>
+</html>
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,3 @@
+<x-layout>
+    <h1>My contact page</h1>
+</x-layout>
