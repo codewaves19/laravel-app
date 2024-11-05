df2db0f40f2a25a813a7fa22352a0f233b7c54d4
manisha <dayalmanisha14@gmail.com>
2024-11-05 13:16:28 +0530

    props and attributes logic in nav links

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,1 +1,3 @@
-    <a {{ $attributes }}>{{ $slot }}</a>
+    <!--<a {{ $attributes }}>{{ $slot }}</a>-->
+    @props(['active'])
+    <a class="{{ request()->is('/') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} rounded-md px-3 py-2 text-sm font-medium" area-current="{{ request()->is('/') ? 'page' : 'false' }}" {{ $attributes }}>{{ $slot }}</a>
