0a75f621bb0d1f3b09718083a0d96e2d4bcdf68e
manisha <dayalmanisha14@gmail.com>
2024-11-06 17:10:48 +0530

    jobs with each job details page implemented

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Arr;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,12 +14,58 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home', ['name' => 'Samantha']);
+    return view('home');
 });
-Route::get('/about', function () {
+Route::get('/jobs', function () {
     //return '<h1>Hello from about page.</h1>';
     //return ['foo' => 'bar']; // return array
-    return view('about');
+    return view('jobs', [
+        'jobs' => [
+            [
+                'id' => 1,
+                'title' => 'Director',
+                'salary' => '$50,000'
+            ],
+            [
+                'id' => 2,
+                'title' => 'Programer',
+                'salary' => '$150,000'
+            ],
+            [
+                'id' => 3,
+                'title' => 'Teacher',
+                'salary' => '$80,000'
+            ]
+        ]
+    ]);
+});
+Route::get('/jobs/{id}', function ($id) {
+    $jobs = [
+        [
+        'id' => 1,
+        'title' => 'Director',
+        'salary' => '$50,000'
+        ],
+        [
+            'id' => 2,
+            'title' => 'Programer',
+            'salary' => '$150,000'
+        ],
+        [
+            'id' => 3,
+            'title' => 'Teacher',
+            'salary' => '$80,000'
+        ]
+        ];
+   /* Arr::first($jobs, function ($job) use ($id) {
+        if ($job['id'] == $id){
+            return dd($job);
+        };
+    });*/
+
+   $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
+    //dd($job);
+   return view('job', ['job' => $job]);
 });
 
 Route::get('/contact', function () {
