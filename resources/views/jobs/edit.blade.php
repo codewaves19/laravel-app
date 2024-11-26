ffc7b45f810e90aecc45e01dc23e2629735ae16a
manisha <dayalmanisha14@gmail.com>
2024-11-27 00:27:12 +0530

    CRUDE operations plus button action on some other form not on its parent form

--- resources/views/jobs/edit.blade.php
+++ resources/views/jobs/edit.blade.php
@@ -4,7 +4,7 @@
     </x-slot:heading>
 <form method="POST" action="/jobs/{{ $job->id }}">
     @csrf
-    @method('PATCH')
+    @method('PATCH') // we have made a post request as browser dont understand patch but used method patch which is known to laravel, this directive expands to a hidden input and laravel will read that input automatically and will hit patch route automatically
     <div class="space-y-12">
   
         <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
@@ -46,11 +46,21 @@
           </div>
         </div>
       </div>
-    </div>
   
-    <div class="mt-6 flex items-center justify-end gap-x-6 mr-16">
-      <a href="/jobs/{{ $job->id }}" class="text-sm/6 font-semibold text-gray-900">Cancel</a>
-      <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Update</button>
+    <div class="mt-6 flex items-center justify-between gap-x-6">
+      <div class="flex items-center">
+        <button id="delete-form" class="text-red-500 text-sm font-bold">Delete</button>
+      </div>
+      <div class="flex items-center justify-end gap-x-6">
+        <a href="/jobs/{{ $job->id }}" class="text-sm font-semibold text-gray-900">Cancel</a>
+        <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Update</button>
+      </div>
     </div>
+  </div>
+
+  </form>
+  <form method="POST" action="/jobs/{{ $job->id }}" id="delete-form" class="hidden">
+    @csrf // token to gurantee that it is coming from autheticated site
+    @method('DELETE')
   </form>
   </x-layout>
