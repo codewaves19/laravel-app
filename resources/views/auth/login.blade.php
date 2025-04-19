b24737eb6c99f27019fcf77d5adb48130445111f
manisha <dayalmanisha14@gmail.com>
2025-04-19 22:22:42 +0530

    created Log In  form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,26 +1,13 @@
 <x-layout>
     <x-slot:heading>
-        Register </x-slot:heading>
-    <form method="POST" action="\jobs">
+        Log In </x-slot:heading>
+    <form method="POST" action="">
         @csrf
         <div class="space-y-12">
             <div class="border-b border-gray-900/10 pb-12">
 
                 <div class="grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                    <x-form-field>
-                        <x-form-label for="first_name">First Name</x-form-label>
-                        <div class="mt-2">
-                            <x-form-input type="text" name="first_name" id="first_name" required />
-                            <x-form-error name="first_name" />
-                        </div>
-                    </x-form-field>
-                    <x-form-field>
-                        <x-form-label for="last_name">Last Name</x-form-label>
-                        <div class="mt-2">
-                            <x-form-input type="text" name="last_name" id="last_name" required />
-                            <x-form-error name="last_name" />
-                        </div>
-                    </x-form-field>
+
 
                     <x-form-field>
                         <x-form-label for="email">Email</x-form-label>
@@ -37,11 +24,9 @@
                         </div>
                     </x-form-field>
                     <x-form-field>
-                        <x-form-label for="password_confirmation">Confirm Password</x-form-label>
+                        <x-form-label for="remember_me">Remember Me</x-form-label>
                         <div class="mt-2">
-                            <x-form-input type="password" name="password_confirmation" id="password_confirmation"
-                                required />
-                            <x-form-error name="password_confirmation" />
+                            <x-form-input type="checkbox" name="remember_me" id="remember_me" />
                         </div>
                     </x-form-field>
                 </div>
@@ -50,7 +35,7 @@
 
         <div class="mt-6 flex items-center justify-end gap-x-6">
             <a href="/" class="text-sm/6 font-semibold text-gray-900">Cancel</a>
-            <x-form-button type="submit">Register</x-form-button>
+            <x-form-button type="submit">Log In</x-form-button>
         </div>
     </form>
 </x-layout>
